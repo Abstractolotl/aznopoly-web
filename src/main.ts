13be174d98982c019a00811273b8841767ded7c6
dseydel <daniel.seydel@ionos.com>
2024-04-26 10:12:27 +0200

    Try fixing issue when moving to lobby screen

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,7 @@ import * as THREE from 'three';
 //import * as WebGLDebugUtils from './webgl-debug.js';
 import DebugScene from './phaser/scenes/debug-scene.js';
 import {DiscordClient} from "@/util/discord.ts";
+import {RoomEvent} from "@/room.ts";
 
 window.onload = () => {
     setTimeout(async () => {
@@ -82,19 +83,18 @@ window.onload = () => {
         const params = new URLSearchParams(window.location.search);
         const debug = params.get('debug');
 
-        if (params.get('frame_id') !== null) {
-            console.log("Discord embedded frame detected");
-            let discordClient = new DiscordClient();
-            await discordClient.handleAuthentication();
-
-            aznopoly.init(discordClient.getRoomId())
-            game.scene.start('lobby')
-        }
-
         if (debug !== null) {
             console.log('Debug mode enabled', debug);
             mock(aznopoly);
             game.scene.start(debug || 'lobby');
+        } else if (params.get('frame_id') !== null) {
+            let discordClient = new DiscordClient();
+            await discordClient.handleAuthentication();
+
+            aznopoly.init(discordClient.getRoomId())
+            aznopoly.room.addEventListener(RoomEvent.READY, () => {
+                game.scene.start('lobby')
+            }, { once: true });
         } else {
             game.scene.start('title');
         }
